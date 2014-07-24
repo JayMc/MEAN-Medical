16b80277844d4dc9d68f727fe00c9d5b27585af0
JayMc <mciver.jason@gmail.com>
2014-07-24 15:17:40 +1000

    Added Angular routes

--- public/src/js/appRoutes.js
+++ public/src/js/appRoutes.js
@@ -1,3 +1,27 @@
 angular.module('appRoutes',[]).config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-	
+
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider
+		.state('home', {
+			url:'/home',
+			templateUrl: 'views/home.html'
+		})
+
+		.state('patients', {
+			url: '/patients',
+			views: {
+				'': {
+					templateUrl: 'views/patients/index.html'
+				},
+				'main@patients': {
+					templateUrl: 'views/patients/list.html',
+					controller: 'PatientsListCtrl'
+				},
+				'side@patients': {
+					templateUrl: 'views/patients/details.html',
+					controller: 'PatientsDetailsCtrl'
+				}
+			}
+		})
 }])
