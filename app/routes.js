64bbea6b7c8b1c3a637cd21312c16892952f5c37
Jason McIver <applemac@MacBook.local>
2014-07-26 18:02:33 +1000

    Added RESTful endpoint and patient fixture

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,45 @@
 
+var urlPrefix = '/api';
+
 module.exports = function(app,_){
 
+	patientsFixture = [
+		{
+			fname: 'Joe',
+			sname: 'Cocker',
+			age: 20
+		},
+		{
+			fname: 'Bill',
+			sname: 'Murry',
+			age: 25
+		},
+		{
+			fname: 'Micheal',
+			sname: 'Jordan',
+			age: 48
+		},
+		{
+			fname: 'Larry',
+			sname: 'The locksmith',
+			age: 28
+		},
+		{
+			fname: 'Bart',
+			sname: 'Simpson',
+			age: 16
+		},
+		{
+			fname: 'Peter',
+			sname: 'Griffin',
+			age: 45
+		}
+	];
+
 	//Rest routes
+	app.get(urlPrefix+'/patients', function(req,res){
+		res.json(patientsFixture)
+	})
 
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
