8ad00a87ae8a5ed8362657e99279651569c9687c
JayMc <mciver.jason@gmail.com>
2014-07-28 12:43:05 +1000

    added patient and appointment end points

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,7 @@
 
+var Patient = require('./models/patient');
+var Appointment = require('./models/appointment');
+
 var urlPrefix = '/api';
 
 module.exports = function(app,_){
@@ -36,10 +39,77 @@ module.exports = function(app,_){
 		}
 	];
 
-	//Rest routes
+////////////////////////////////////////////////
+//Rest routes
+	
+	//find all patients
 	app.get(urlPrefix+'/patients', function(req,res){
-		res.json(patientsFixture)
+		//res.json(patientsFixture)
+		Patient.find(function(err, patients){
+			if(err) return console.error(err);
+				res.json(patients);
+		})
 	})
+
+	//find all appointments for a given patient
+	app.get(urlPrefix+'/patients/:id/appointments', function(req,res){
+		//If patient had an array of appointment refs we could just use populate on the patient.appointments ref
+		//But that gives us a second set of reference pointers that could get out of sync if there was a change between patients and appointments
+		//Its safer to do a find of all appointments with the patient_id
+		Appointment
+			.find({_patient: req.params.id})
+			.populate('_patient')
+			.exec(function(err, appointments){
+			if(err) return console.error(err);
+				res.json(appointments);
+		})
+	})
+
+	//create a patient
+	app.post(urlPrefix+'/patients', function(req,res){
+		var createPatient = new Patient({
+			fname: req.body.fname,
+			sname: req.body.sname,
+			age: req.body.age
+		});
+		createPatient.save(function(err, patients){
+			if(err) return console.error(err);
+
+			res.json(createPatient);
+		})
+	})
+
+
+//////////////////////////////
+//Appointments
+
+	//get all appointments
+	app.get(urlPrefix+'/appointments', function(req,res){
+		Appointment
+			.find()
+			.populate('_patient')
+			.exec(function(err, appointments){
+				if(err) return console.error(err);
+					res.json(appointments);
+			})
+	})
+
+	//create an appointment
+	app.post(urlPrefix+'/appointments', function(req,res){
+		var createAppointment = new Appointment({
+			_patient: req.body.patient_id,
+			type: req.body.type
+		});
+		createAppointment.save(function(err, appointments){
+			if(err) return console.error(err);
+
+			res.json(createAppointment);
+		})
+	})
+
+
+//////////////////////////////
+//static routes
 
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
